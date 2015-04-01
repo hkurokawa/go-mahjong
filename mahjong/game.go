00d41a4b6c25f964cc62ce287a9a98630878397e
hkurokawa <hiroshi.kurokawa@gunosy.com>
2015-04-02 01:01:43 +0900

    Rename const variables with Kanji to make them public.

--- mahjong/game.go
+++ mahjong/game.go
@@ -3,10 +3,10 @@ package mahjong
 type Kaze rune
 
 const (
-	東風 Kaze = '東'
-	南風      = '南'
-	西風      = '西'
-	北風      = '北'
+	TongPu Kaze = '東'
+	NangPu = '南'
+	ShaPu = '西'
+	PeiPu = '北'
 )
 
 type Order Kaze
